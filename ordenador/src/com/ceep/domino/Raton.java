05932fe1a605844c9718b1b317c006de5fe1ecd1
Maria-Luengo <77382852+Maria-Luengo@users.noreply.github.com>
2021-10-06 12:23:20 +0200

    Update Raton.java

--- ordenador/src/com/ceep/domino/Raton.java
+++ ordenador/src/com/ceep/domino/Raton.java
@@ -1,36 +1,19 @@
-
 package com.ceep.domino;
 
 //Agrega a Dispositivos de entrada
-public class Raton extends DispositivoEntrada{
-    
-    private int idRaton;
-    private static int contadorRatones;
+public class Raton extends DispositivoEntrada {
 
-    public Raton(String tipoEntrada, String marca, int contadorRatones) {
-        super(tipoEntrada, marca);
-        this.contadorRatones=++Raton.contadorRatones;
-    }
-    
-    public Raton(int idRaton, int contadorRatones, String tipoEntrada, String marca) {
-        
-        super(tipoEntrada, marca);
-        this.idRaton = idRaton;
-        this.contadorRatones = contadorRatones;
-    }
+    private final int idRaton;
+    private static int contadorRatones;
 
     public Raton(String tipoEntrada, String marca) {
         super(tipoEntrada, marca);
+        this.idRaton = ++Raton.contadorRatones;
     }
-    
-    
-
-    
 
     @Override
     public String toString() {
-        return "Raton{" + "idRaton=" + idRaton + ", contadorRatones=" + contadorRatones + '}';
+        return super.toString()+"idRaton=" + idRaton;
+        //añadimos el toString de la clase padre
     }
-    
-    
 }
