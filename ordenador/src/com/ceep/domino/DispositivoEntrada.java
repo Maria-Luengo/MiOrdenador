cf1ca279e473560280a3550bc87df41cb3b723b9
Maria-Luengo <77382852+Maria-Luengo@users.noreply.github.com>
2021-10-06 10:59:22 +0200

    Create DispositivoEntrada.java

---
+++ ordenador/src/com/ceep/domino/DispositivoEntrada.java
@@ -0,0 +1,30 @@
+package com.ceep.domino;
+
+public class DispositivoEntrada  {
+
+    private String tipoEntrada;
+    private String marca;
+
+    public DispositivoEntrada(String tipoEntrada, String marca) {
+        this.tipoEntrada = tipoEntrada;
+        this.marca = marca;
+    }
+
+    public String getTipoEntrada() {
+        return tipoEntrada;
+    }
+
+    public void setTipoEntrada(String tipoEntrada) {
+        this.tipoEntrada = tipoEntrada;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+    
+
+}
