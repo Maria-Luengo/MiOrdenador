1d02c4b58efd08b180aa4799af2f41dc50e7f00a
Maria-Luengo <77382852+Maria-Luengo@users.noreply.github.com>
2021-10-06 12:23:22 +0200

    Update Orden.java

--- ordenador/src/com/ceep/domino/Orden.java
+++ ordenador/src/com/ceep/domino/Orden.java
@@ -1,30 +1,31 @@
-
 package com.ceep.domino;
 
 public class Orden {
-   private int idOrden;
-   Computadora computadoras[];
-   private static int contadorOrdenes;
-   private static int contadorComputadoras;
-   private final static int  maxComputadoras =20;
+
+    private final int idOrden;
+    Computadora computadoras[];
+    private static int contadorOrdenes;
+    private static int contadorComputadoras;
+    private final static int maxComputadoras = 4;
 
     public Orden() {
+        this.idOrden = ++contadorOrdenes;
         computadoras = new Computadora[Orden.maxComputadoras];
-        Orden computadoras[] = new Orden[Orden.maxComputadoras];
+        Orden.contadorComputadoras=0; //No pongo this xq es static 
     }
-   
-   public void agregarComputadoras(Computadora ordenador){
-       if(Orden.contadorComputadoras<Orden.maxComputadoras){
-           computadoras[Orden.contadorOrdenes++] = ordenador;
-       }else{
-           System.out.println("Has alcanzado el máximo");
-       }
-       
-   }
-   
-   public void mostrarOrden(){
-       System.out.println("idOrden: "+ idOrden);
-       System.out.println("contador ordenes: "+ contadorOrdenes);
-       System.out.println("contador computadoras: "+ contadorComputadoras);
-   }
+
+    public void agregarComputadoras(Computadora ordenador) {
+        if (Orden.contadorComputadoras < Orden.maxComputadoras) {
+            computadoras[Orden.contadorComputadoras] = ordenador;
+        } else {
+            System.out.println("Has alcanzado el máximo ("+maxComputadoras+")");
+        }
+    }
+
+    public void mostrarOrden() {
+        System.out.println("idOrden: " + this.idOrden); //se puede poner this por buena práctica
+        for (int i = 0; i < contadorComputadoras; i++) {
+            System.out.println(computadoras[i]);
+        }
+    }
 }
