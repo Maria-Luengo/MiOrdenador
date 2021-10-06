0193861edaed6d1b78c0e29ae7a0d785496abf62
Maria-Luengo <77382852+Maria-Luengo@users.noreply.github.com>
2021-10-06 12:23:18 +0200

    Update Teclado.java

--- ordenador/src/com/ceep/domino/Teclado.java
+++ ordenador/src/com/ceep/domino/Teclado.java
@@ -3,27 +3,17 @@ package com.ceep.domino;
 
 //Agrega a Dispositivos de entrada
 public class Teclado extends DispositivoEntrada{
-    private int idTeclado;
-    private int contadorTeclado;
-
-    public Teclado(int idTeclado, int contadorTeclado, String tipoEntrada, String marca) {
-        super(tipoEntrada, marca);
-        this.idTeclado = idTeclado;
-        this.contadorTeclado = ++contadorTeclado;
-    }
+    private final int idTeclado;
+    private static int contadorTeclados;
 
     public Teclado(String tipoEntrada, String marca) {
         super(tipoEntrada, marca);
+        this.idTeclado = ++Teclado.contadorTeclados;
     }
-    
-    
-
-    
 
     @Override
     public String toString() {
-        return "Teclado{" + "idTeclado=" + idTeclado + ", contadorTeclado=" + contadorTeclado + '}';
+        return super.toString()+ "idTeclado=" + idTeclado ;
     }
-    
     
 }
