1cb8a80fe7a9a111be06c54851530f440fa01edb
Maria-Luengo <77382852+Maria-Luengo@users.noreply.github.com>
2021-10-06 12:23:15 +0200

    Update Test.java

--- ordenador/src/com/ceep/test/Test.java
+++ ordenador/src/com/ceep/test/Test.java
@@ -1,4 +1,4 @@
-package com.ceep.pc;
+package com.ceep.test;
 
 import com.ceep.domino.*;
 
@@ -9,9 +9,12 @@ public class Test {
         Raton r1 = new Raton("Usb", "Hp");
         Teclado t1 = new Teclado("Pin", "Hp");
         Monitor m1 = new Monitor("LG", 21.5);
+        Teclado t2 = new Teclado("bloethot", "logitech");
+        System.out.println("t2 = " + t2);
 
-        Computadora c1 = new Computadora(m1, t1, r1);
-        
+        Computadora c1 = new Computadora("pc1", m1, t2, r1);
+
+        //Agregamos un ordenado
         Orden o1 = new Orden();
         o1.agregarComputadoras(c1);
         o1.mostrarOrden();
